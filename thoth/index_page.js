30d80ef7fbd62bcff6965a26660c899c4e133457
Camilla Marie Dalan <camillamariedalan@dhcp-10-22-36-95.wlan.ntnu.no>
2017-02-22 13:08:38 +0100

    Made index page and can access the teacher page from there

---
+++ thoth/index_page.js
@@ -0,0 +1,16 @@
+var express = require('express');
+var http = require('http');
+var fs = require('fs');
+
+fs.readFile('./index_page.html', function (err, html) {
+    if (err) {
+        throw err;
+    }
+    http.createServer(function(request, response) {
+        response.writeHeader(200, {"Content-Type": "text/html"});
+        response.write(html);
+        response.end();
+    }).listen(8000);
+});
+
+console.log('Server running on port 8000.');
