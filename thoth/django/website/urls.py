bbf002995e1a847efabbfd77581536087a36b861
helemork <helemork@gmail.com>
2017-03-08 12:07:18 +0100

    restructured a bit and made things work with node too

--- thoth/django/website/urls.py
+++ thoth/django/website/urls.py
@@ -19,7 +19,7 @@ from . import views
 
 urlpatterns = [
 
-    url(r'^thoth/$', views.index, name='index'),
+    url(r'^/*$', views.index, name='index'),
     url(r'^student/$', views.student, name='student'),
     url(r'^teacher/$', views.teacher, name='teacher'),
     url(r'^login/$', views.login1, name="login"),
--- thoth/django/website/views.py
+++ thoth/django/website/views.py
@@ -13,14 +13,14 @@ def index (request):
     return render(request, 'index.html')
 
 def student(request):
-    return render(request, 'student.html')
+    return render(request, 'student/student.html')
 
 def teacher(request):
-    return render(request, 'teacher.html')
+    return render(request, 'teacher/teacher.html')
 
 class UserFormView(View):
     form_class = Userform
-    template_name = 'registration_form.html'
+    template_name = 'teacher/registration_form.html'
 
         #Displays blank form
     def get(self, request):
@@ -57,7 +57,7 @@ class UserFormView(View):
 
         return render(request, self.template_name, {'form': form})
 
-#skal egentlig ha render_to_response, men fikk hele tiden error når jeg brukte det, skjønner ikke hvorfor
+#skal egentlig ha render_to_response, men fikk hele tiden error nï¿½r jeg brukte det, skjï¿½nner ikke hvorfor
 def login1(request):
     msg = []
     if request.method == 'POST':
@@ -73,4 +73,4 @@ def login1(request):
                 msg.append("disabled account")
     else:
         msg.append("invalid login")
-    return render(request, 'login.html')
+    return render(request, 'teacher/login.html')
