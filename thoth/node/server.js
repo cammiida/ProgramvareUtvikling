ed2da117a9c6c8daea17d5d85170ca88861345fc
helemork <helemork@gmail.com>
2017-03-21 20:28:32 +0100

    BUGFIX: teacher reload no longer resets studentcount

--- thoth/node/server.js
+++ thoth/node/server.js
@@ -49,16 +49,25 @@ io.on('connection',function(socket){
               connectedstudents.splice(i,1);
             }
           };
-          io.to(lectures[lectureid].teacherid).emit('update',feedbackcalculator(lectureid));
+        io.to(lectures[lectureid].teacherid).emit('update',feedbackcalculator(lectureid));
       });
     }
     else{
       console.log('Teacher has logged on with lecture ' + lectureid);
       // detects the socket id from the teacher connection and sets it.
-      lectures[lectureid] = {
-        teacherid:socket.id,
-        students:[],
-      };
+      if (lectureid in lectures){
+        lectures[lectureid].teacherid = socket.id;
+        console.log('lecture existed');
+        io.to(lectures[lectureid].teacherid).emit('update',feedbackcalculator(lectureid));
+      }
+      else{
+        lectures[lectureid] = {
+          teacherid:socket.id,
+          students:[],
+        };
+        console.log('lecture created');
+      }
+
       socket.on('disconnect',function(){
         console.log('teacher disconnect');
         // her må vi først lagre dataene våre sånn at de ikke forsvinner.
