5715a2e1bf6e3194aa46ad2b9803538658385941
helemork <helemork@gmail.com>
2017-04-06 17:26:24 +0200

    Add task form and buttons now look good

--- thoth/django/website/static/javascript/header.js
+++ thoth/django/website/static/javascript/header.js
@@ -3,10 +3,26 @@ $(document).ready(function(){
 		$("#liste").toggle();
 	});
 
+	$('#optionsfield').hide();
+	$('#textanswerfield').hide();
 
 	$('#toggleaddtask').click(function(){
 		$('#addtask').slideToggle();
 		console.log('clicks');
 	});
+
+	$('#addtextanswer').click(function(){
+		$('#textanswerfield').slideToggle();
+		$('#optionsfield').hide();
+		console.log('clicks');
+	});
+
+	$('#addoptions').click(function(){
+		$('#optionsfield').slideToggle();
+		$('#textanswerfield').hide();
+		console.log('clicks');
+	});
+
+
 
 });
