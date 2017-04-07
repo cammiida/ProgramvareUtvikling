758b0d842c1cf0bfeeb9dd12cfef8f31a1c68bfe
helemork <helemork@gmail.com>
2017-04-07 23:06:36 +0200

    task history now has graph. cleaned up bugs on activelecture

--- thoth/django/website/static/javascript/teacherfunctions.js
+++ thoth/django/website/static/javascript/teacherfunctions.js
@@ -1,11 +1,49 @@
 
+
 function showhistory(url){
-	$('#taskhistory').load(url);
+	$('#taskhistory').load(url,function(){
+		// Collects the total number of students of each type
+		var corran = $('#corran').text();
+		var worngan = $('#worngan').text();
+		var timeoan = $('#timeoan').text();
+		// Turns strings into numbers
+		corran = parseInt(corran);
+		worngan = parseInt(worngan);
+		timeoan = parseInt(timeoan);
+		// Total number of students
+		var totalnr = (corran+worngan+timeoan);
+		// Calculates percentages
+		var tcorpros = (corran/totalnr)*100;
+		var twropros = (worngan/totalnr)*100;
+		var tiompros = (timeoan/totalnr)*100;
+		// Logs to see it works
+		console.log('corr answers '+corran);
+		console.log('wrong answers  '+worngan);
+		console.log('timed out  answers '+timeoan);
+		console.log('tot '+totalnr);
+		console.log('% '+tcorpros);
+		console.log('% '+twropros);
+		console.log('% '+tiompros);
+
+		// Creates the graph:
+		$('#correctanswers').css('height',tcorpros);
+		$('#wronganswers').css('height',twropros);
+		$('#timedoutanswers').css('height',tiompros);
+	});
 };
 
 
-
 $(document).ready(function(){
+
+	$('#taskhistory').click(function(){
+		$('#taskhistory').hide();
+	})
+
+	$('.addquestion').click(function(){
+		$('#taskhistory').show();
+	})
+
+
 	$('#optionsfield').hide();
 	$('#textanswerfield').hide();
 
